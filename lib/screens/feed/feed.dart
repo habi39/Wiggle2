5d5cfdbb263be2d729dd1df43610932dcfab7736
maguireong <maguire2622@yahoo.com.sg>
2020-06-18 20:50:23 +0800

    ..

--- lib/screens/feed/feed.dart
+++ lib/screens/feed/feed.dart
@@ -155,13 +155,6 @@ class _FeedState extends State<Feed> {
           UserData userData = snapshot.data;
           return Scaffold(
             appBar: AppBar(
-              // leading: IconButton(
-              //     icon: Icon(LineAwesomeIcons.home),
-              //     onPressed: () {
-              //       Navigator.of(context).pushAndRemoveUntil(
-              //           FadeRoute(page: Wrapper()),
-              //           ModalRoute.withName('Wrapper'));
-              //     }),
               title: Text("F E E D",
                   textAlign: TextAlign.right,
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w100)),
@@ -181,31 +174,6 @@ class _FeedState extends State<Feed> {
   }
 }
 
-// displayUploadScreen(context) {
-//   return Container(
-//       color: Colors.blueGrey,
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: <Widget>[
-//           Icon(Icons.add_photo_alternate, color: Colors.red, size: 200),
-//           Padding(
-//               padding: EdgeInsets.only(top: 20),
-//               child: RaisedButton(
-//                 color: Colors.red,
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(9),
-//                 ),
-//                 child: Text(
-//                   "Upload Image",
-//                   style: TextStyle(color: Colors.white, fontSize: 20),
-//                 ),
-//                 onPressed: () => takeImage(context),
-//               ))
-//         ],
-//       ));
-// }
-
 class FeedTile extends StatelessWidget {
   final Wiggle wiggle;
   final List<Wiggle> wiggles;
@@ -222,29 +190,6 @@ class FeedTile extends StatelessWidget {
     this.url,
   });
 
-  // Widget getLatestTime() {
-  //   DatabaseService().getConversationMessages(chatRoomId).then((val) {
-  //     chatMessagesStream = val;
-  //   });
-  //   return StreamBuilder(
-  //     stream: chatMessagesStream,
-  //     builder: (context, snapshot) {
-  //       if (snapshot.hasData) {
-  //         if (snapshot.data.documents.length - 1 < 0) {
-  //           return Text('');
-  //         } else {
-  //           return Text(f
-  //               .format(snapshot.data
-  //                   .documents[snapshot.data.documents.length - 1].data["time"]
-  //                   .toDate())
-  //               .toString());
-  //         }
-  //       } else {
-  //         return Container();
-  //       }
-  //     },
-  //   );
-  // }
   createPostHead(context) {
     return Container(
       height: 50,
