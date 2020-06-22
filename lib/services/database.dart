644bc999025f47ea7de0f44cdcfd88681b2dfebf
habi39 <65403285+habi39@users.noreply.github.com>
2020-06-22 11:40:44 +0800

    not needed

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -258,10 +258,6 @@ class DatabaseService {
         .setData({'photo': photo});
   }
 
-  Future deletephoto(String photo) async{
-    return await wiggleCollection.document(uid).collection('photos').document(photo).delete();
-  }
-
   Stream<QuerySnapshot> getphotos() {
     return wiggleCollection.document(uid).collection('photos').snapshots();
   }
